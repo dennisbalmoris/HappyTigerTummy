4e936bd8e3afd171430923546f13f63aea200568
rafaelatabug123 <2015081032@ust-ics.mygbiz.com>
2018-11-23 21:36:30 +0800

    Added Image Upload in Profile Screen

--- app/src/main/java/dennis/balmoris/com/happytiger/ProfileActivity.java
+++ app/src/main/java/dennis/balmoris/com/happytiger/ProfileActivity.java
@@ -1,7 +1,10 @@
 package dennis.balmoris.com.happytiger;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
@@ -9,9 +12,24 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -19,12 +37,42 @@ public class ProfileActivity extends AppCompatActivity {
     private DrawerLayout dl;
     private ActionBarDrawerToggle adbt;
 
+    ImageView imageView;
+    EditText editText;
+    ProgressBar progressBar;
+    Uri uriProfileImage;
+    String profileImageUrl;
+
+    FirebaseAuth mAuth;
+
     private static final int CHOOSE_IMAGE = 101;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        editText = (EditText) findViewById(R.id.editTextDisplayName);
+        imageView = (ImageView) findViewById(R.id.profileImage);
+        progressBar = findViewById(R.id.progressBar);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showImageChooser();
+            }
+        });
+
+        findViewById(R.id.btnProceed).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveUserInformation();
+
+            }
+        });
+
+        //Drawer Layout
         dl = (DrawerLayout)findViewById(R.id.dl);
         adbt = new ActionBarDrawerToggle(this, dl, R.string.Open,R.string.Close);
         adbt.setDrawerIndicatorEnabled(true);
@@ -76,6 +124,88 @@ public class ProfileActivity extends AppCompatActivity {
         });
     }
 
+    private void saveUserInformation() {
+        String displayName = editText.getText().toString();
+
+        if(displayName.isEmpty()){
+            editText.setError("Name required");
+            editText.requestFocus();
+
+            return;
+        }
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if(user != null && profileImageUrl != null){
+            UserProfileChangeRequest profile = new UserProfileChangeRequest.Builder()
+                    .setDisplayName(displayName)
+                    .setPhotoUri(Uri.parse(profileImageUrl))
+                    .build();
+
+            user.updateProfile(profile)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(ProfileActivity.this, "Profile updated", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == CHOOSE_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null){
+
+            uriProfileImage = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uriProfileImage);
+                imageView.setImageBitmap(bitmap);
+
+                uploadImagetoFireBaseStorage();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void uploadImagetoFireBaseStorage() {
+        final StorageReference profileImageRef = FirebaseStorage.getInstance().getReference("profilepics/"+System.currentTimeMillis()+".jpg");
+
+        if(uriProfileImage != null){
+            progressBar.setVisibility(View.VISIBLE);
+            profileImageRef.putFile(uriProfileImage)
+                  .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                      @Override
+                      public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                          progressBar.setVisibility(View.GONE);
+                          profileImageUrl = taskSnapshot.getMetadata().getReference().getDownloadUrl().toString();
+                      }
+                  })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressBar.setVisibility(View.GONE);
+
+                            Toast.makeText(ProfileActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+
+    private void showImageChooser(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Profile Image"), CHOOSE_IMAGE);
+
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return adbt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
@@ -83,13 +213,7 @@ public class ProfileActivity extends AppCompatActivity {
 
 
 
-   private void showImageChooser(){
-        Intent intent = new Intent();
-        intent.setType("image/");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Profile Image"), CHOOSE_IMAGE);
 
-    }
 
 
 }
